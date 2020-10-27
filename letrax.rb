ce3b1cc605f273bad8f99702e3ae04aec6f15be1
Francisca <fvargasserani@gmail.com>
2020-10-27 13:37:16 -0300

    letrax

--- letrax.rb
+++ letrax.rb
@@ -1,9 +1,42 @@
 n = ARGV[0].to_i
+i = 1
 
-n.times do |i|
-    (n-i-1).times do |i|
+#parte superior
+(n/2).times do |i|
+    i.times do |i|
         print ' '
     end
-        print '*'
-        print "\n"
+print '*'
+if n.odd?
+    ((n-(2*i))-2).times do |i|
+        print ' '
+    end
+else
+    ((n-(2*i))-1).times do |i|
+        print ' '
+        end
+    end
+print '*'
+print "\n"
 end
+
+#parte intermedia
+(n/2).times do |i|
+    print ' ' 
+end
+print '*'
+print "\n"
+
+#parte inferior
+(n/2).times do |i|
+    ((n/2)-(i+1)).times do |i|
+        print ' '
+    end
+print '*'
+    (i+(i+1)).times do |i|
+        print ' '
+    end
+print '*'
+print "\n"
+end
+
