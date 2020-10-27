3b1c6c35aa7dc72c34669f328f4468d189fe7f46
Francisca <fvargasserani@gmail.com>
2020-10-26 21:58:06 -0300

    letraz

---
+++ letraz.rb
@@ -0,0 +1,25 @@
+#Parte superior
+n = ARGV[0].to_i
+n.times do |i|
+    print '*'
+end
+print "\n"
+
+#Parte intermedia
+n = ARGV[0].to_i
+(n-2).times do |i|
+    (n-i-2).times do |i|
+        print ' '
+    end
+        print '*'
+    (n-i).times do |i|
+        print ' '
+    end
+        print "\n"
+end
+
+#Parte inferior
+n.times do |i|
+    print '*'
+end
+print "\n"
